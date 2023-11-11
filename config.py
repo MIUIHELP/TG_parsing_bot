36bb1e481315fbe03fedd7cdb03ff3a1ef28934e
MIUIHELP <50364584+MIUIHELP@users.noreply.github.com>
2023-11-11 13:22:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-username = 'Yama.S'
-password = 'daa47b973c7c'
-token = '5346150204:AAFW8ezSZHzih6V2YcQ-dHPNZP9_29yJ5RQ'
-chat_id = '-1001920722839'
+username = 'user_name'
+password = 'pssw'
+token = 'BOT_TOKEN'
+chat_id = 'group_chat_id'
